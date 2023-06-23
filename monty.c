6a87986a342c424ec065cedcdf9ef5fd84abf87c
beekaz <sooperfect00@gmail.com>
2023-06-22 17:19:49 -0700

    program to execute monty program

---
+++ monty.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+
+int main(void)
+{
+    char *opcode;
+    int value, line_number = 1;
+    size_t bufsize = 0;
+    char *line = NULL;
+    ssize_t linelen;
+
+    while ((linelen = getline(&line, &bufsize, stdin)) != -1)
+    {
+        opcode = strtok(line, " \n\t\r");
+        if (opcode == NULL || *opcode == '#')
+        {
+            line_number++;
+            continue;
+        }
+
+        if (strcmp(opcode, "push") == 0)
+        {
+            char *arg = strtok(NULL, " \n\t\r");
+            if (arg == NULL)
+            {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                exit(EXIT_FAILURE);
+            }
+
+            value = atoi(arg);
+	    push(&stack, value);
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+		pall(&stack);
+        }
+        else if (strcmp(opcode, "pint") == 0)
+        {
+		pint(&stack, line_number);
+        }
+        else
+        {
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+
+        line_number++;
+    }
+
+    free(line);
+    return 0;
+}
