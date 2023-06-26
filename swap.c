fb8fcb271f8d5e8c9300f5cc14fbaae010848e3f
beekaz <sooperfect00@gmail.com>
2023-06-25 18:39:56 -0700

    The opcode swap swaps the top two elements of the stack.

--- swap.c
+++ swap.c
@@ -1,16 +1,16 @@
 #include "monty.h"
 /**
- * f_swap - adds the top two elements of the stack.
+ * swap - adds the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_swap(stack_t **head, unsigned int counter)
+void swap(__attribute((unused)) stack_t **head, __attribute((unused)) unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
-
 	h = *head;
+	
 	while (h)
 	{
 		h = h->next;
