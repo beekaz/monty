76b7d8f6a356444bcf36ae05c2b4a7a36b5bd918
beekaz <sooperfect00@gmail.com>
2023-06-25 18:19:20 -0700

    added uses of array, and arranged the functions

--- execute.c
+++ execute.c
@@ -7,47 +7,45 @@
 * @content: line content
 * Return: no return
 */
-int  execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
-	char *opcode;
-	int value;
-	
-	opcode = strtok(content, " \t\n");
-	if (opcode == NULL || opcode[0] == '#')
+	instruction_t opst[] = {
+				{"push", push}, {"pall", pall}, {"pint", pint},
+				{"pop", pop},
+				{"swap", swap},
+				{"add", add},
+				{"sub", sub},
+				{"div", divide},
+				{"mul", mul},
+				{"mod", mod},
+				{"pchar", pchar},
+				{"pstr", pstr},
+				{"rotl", rotl},
+				{"rotr", rotr},
+				{"queue", queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
+
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
 	{
-		return 0;
-	}
-	if (strcmp(opcode, "push") == 0)
-	{
-		char *arg = strtok(NULL, " \t\n");
-		if (arg == NULL)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(file);
-			free_stack(*stack);
-			exit(EXIT_FAILURE);
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
 		}
-		if (!is_numeric(arg))
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(file);
-			free_stack(*stack);
-			exit(EXIT_FAILURE);
-		}
-		value = atoi(arg);
-		push(stack, value);
+		i++;
 	}
-	else if (strcmp(opcode, "pall") == 0)
-	{
-	    /*stack_t **head = NULL;*/
-		pall(stack, counter);
-	}
-	else
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", counter, opcode);
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
 		fclose(file);
+		free(content);
 		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	return -1;
+		exit(EXIT_FAILURE); }
+	return (1);
 }
