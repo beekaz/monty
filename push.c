edf66094701c5c7d8cd3c3c90dd7fe0b288a79c0
beekaz <sooperfect00@gmail.com>
2023-06-25 18:29:57 -0700

    monty

--- push.c
+++ push.c
@@ -8,19 +8,31 @@
 */
 void push(stack_t **head, unsigned int counter)
 {
-	 stack_t *new_node;
-	 new_node = malloc(sizeof(stack_t));
-	 if (new_node == NULL)
-		 {
-			 fprintf(stderr, "Error: Failed to allocate memory\n");
-			 exit(EXIT_FAILURE);
-		 }
-	 new_node->n = counter;
-	 new_node->next = *head;
-	 new_node->prev = NULL;
-	 if (*head != NULL)
-	 {
-		 (*head)->prev = new_node;
-		 *head = new_node;
-	 }
+	int n, j = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
